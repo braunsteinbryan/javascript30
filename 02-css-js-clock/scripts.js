5c9cd765dbf985550a97caa0439fcd5cbe46937d
braunsteinbryan <braunsteinbryan@gmail.com>
2021-06-22 13:39:23 -0400

    created project for javascript 30 - day2

---
+++ 02-css-js-clock/scripts.js
@@ -0,0 +1,24 @@
+function setDate() {
+    const secondHand = document.querySelector('.second-hand');
+    const minHand = document.querySelector('.min-hand');
+    const hourHand = document.querySelector('.hour-hand');
+
+    const now = new Date();
+
+    const seconds = now.getSeconds();
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+    console.log(seconds);
+
+    const mins = now.getMinutes();
+    const minsDegrees = ((mins / 60) * 360) + 90;
+    minHand.style.transform = `rotate(${minsDegrees}deg)`;
+    console.log(mins);
+
+    const hours = now.getHours();
+    const hoursDegrees = ((hours / 12) * 360) + 90;
+    hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+    console.log(hours);
+}
+
+setInterval(setDate, 1000);
